88ce49d1d615602b9a77c84589be518d2f8d2411
sitek94 <msitkowski94@gmail.com>
2021-02-21 16:19:34 +0100

    create basic layout

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,9 +1,33 @@
+import styled from 'styled-components'
+import { Button, Row, Col } from 'antd'
+
 function App() {
   return (
-    <div>
-      <h1>Simple Notes App</h1>
-    </div>
+    <Container>
+      <Row justify="space-between" align="middle">
+        <h1>Simple Notes App</h1>
+        <Button type="primary">New Note</Button>
+      </Row>
+      <Row>
+        <Col span={8}>
+          <div>
+            TODO: Notes List Component
+          </div>
+        </Col>
+        <Col span={16}>
+          <form>
+            TODO: 
+          </form>
+        </Col>
+      </Row>
+    </Container>
   )
 }
+
+const Container = styled.div`
+  height: 100vh;
+  max-width: 1000px;
+  margin: 0 auto;
+`
 
 export { App }
