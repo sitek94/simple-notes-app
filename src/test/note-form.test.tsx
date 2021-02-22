e7a57160b300f95bdb3a529135af800954ff4d93
sitek94 <msitkowski94@gmail.com>
2021-02-22 18:00:39 +0100

    check if input fields are displayed correctly

--- src/test/note-form.test.tsx
+++ src/test/note-form.test.tsx
@@ -5,6 +5,20 @@ import notes from './notes.json'
 import { NoteForm } from '../components/note-form'
 
 describe('NoteForm Component', () => {
+  it('should display title and note form input fields after note is selected', () => {
+    // given
+    const note = notes[0]
+    render(<NoteForm note={note} onSubmit={() => {}} onCancel={() => {}} />)
+
+    // then
+    expect(screen.getByRole('textbox', { name: /title/i })).toHaveValue(
+      note.title
+    )
+    expect(screen.getByRole('textbox', { name: /note/i })).toHaveValue(
+      note.text
+    )
+  })
+
   it('should call onSubmit with changed note after the form is submitted', async () => {
     // given
     const onSubmit = jest.fn()
